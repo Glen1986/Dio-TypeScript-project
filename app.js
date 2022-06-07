4701fff68d207a8bafe8fc7c4df1a90c6416cfeb
Glen1986 <escobar.glen@gmail.com>
2022-06-07 20:02:11 -0300

    typeof-salida-de-function-sin-retorno

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ function sumarValoresNumericos(n1, n2) {
     return n1 + n2;
 }
 console.log(sumarValoresNumericos(4, 4));
+//retorno de tipo "void", es cuando la funcion no retorna nada
+function printaValoresNumericos(n1, n2) {
+    console.log(n1 + n2);
+}
--- app.ts
+++ app.ts
@@ -3,3 +3,8 @@ function sumarValoresNumericos(n1: number, n2: number): number {
     return n1 + n2
 }
 console.log(sumarValoresNumericos(4, 4))
+
+//retorno de tipo "void", es cuando la funcion no retorna nada
+function printaValoresNumericos(n1: number, n2: number): void {
+    console.log(n1 + n2)
+}
