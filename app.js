cc94a93af2f5a28163d930b18f04c47e9e13f290
Glen1986 <escobar.glen@gmail.com>
2022-06-07 20:29:18 -0300

    parametro-calback

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 "use strict";
-//                          definimos el typo de in/  definimos el tipo de salida de la funcion
-function sumarValoresNumericos(n1, n2) {
-    return n1 + n2;
-}
-console.log(sumarValoresNumericos(4, 4));
 //retorno de tipo "void", es cuando la funcion no retorna nada
 function printaValoresNumericos(n1, n2) {
     console.log(n1 + n2);
 }
+function sumarValoresNumericosETratar(n1, n2, callback) {
+    let resultado = n1 + n2;
+    return callback(resultado);
+}
+function alCuadrado(numero) {
+    return numero * numero;
+}
+console.log(sumarValoresNumericosETratar(1, 3, alCuadrado));
--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
-//                          definimos el typo de in/  definimos el tipo de salida de la funcion
-function sumarValoresNumericos(n1: number, n2: number): number {
-    return n1 + n2
-}
-console.log(sumarValoresNumericos(4, 4))
-
 //retorno de tipo "void", es cuando la funcion no retorna nada
 function printaValoresNumericos(n1: number, n2: number): void {
     console.log(n1 + n2)
 }
+
+function sumarValoresNumericosETratar(
+    n1: number,
+    n2: number,
+    callback: (n3: number) => number
+): number {
+    let resultado = n1 + n2
+    return callback(resultado)
+}
+function alCuadrado(numero: number): number {
+    return numero * numero
+}
+console.log(sumarValoresNumericosETratar(1, 3, alCuadrado))
