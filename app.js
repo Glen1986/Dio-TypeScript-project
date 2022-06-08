7c295b35972194208029be54f933b8c412415fe5
Glen1986 <escobar.glen@gmail.com>
2022-06-07 21:10:16 -0300

    never-type-return

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ let anyEstaDevuelta;
 anyEstaDevuelta = 3;
 anyEstaDevuelta = true;
 anyEstaDevuelta = 'string';
+anyEstaDevuelta = 5;
 let stringTest = 'verificar';
 stringTest = anyEstaDevuelta;
-let unknowValue;
-unknowValue = 3;
-unknowValue = 'opa';
-unknowValue = false;
-unknowValue = 'no es';
+let unknownValue;
+unknownValue = 3;
+unknownValue = 'opa';
+unknownValue = false;
+unknownValue = 'no es';
 let stringTest2 = 'ahora es';
-stringTest2 = unknowValue;
+if (typeof unknownValue === 'string') {
+    stringTest2 = unknownValue;
+}
+function tiraError(erro, codigo) {
+    throw { error: erro, code: codigo };
+}
+tiraError('deu erro', 500);
--- app.ts
+++ app.ts
@@ -2,15 +2,24 @@ let anyEstaDevuelta: any
 anyEstaDevuelta = 3
 anyEstaDevuelta = true
 anyEstaDevuelta = 'string'
+anyEstaDevuelta = 5
 
 let stringTest: string = 'verificar'
 stringTest = anyEstaDevuelta
 
-let unknowValue: unknown
-unknowValue = 3
-unknowValue = 'opa'
-unknowValue = false
-unknowValue = 'no es'
+let unknownValue: unknown
+unknownValue = 3
+unknownValue = 'opa'
+unknownValue = false
+unknownValue = 'no es'
 
 let stringTest2: string = 'ahora es'
-stringTest2 = unknowValue
+
+if (typeof unknownValue === 'string') {
+    stringTest2 = unknownValue
+}
+
+function tiraError(erro: string, codigo: number): never {
+    throw { error: erro, code: codigo }
+}
+tiraError('deu erro', 500)
